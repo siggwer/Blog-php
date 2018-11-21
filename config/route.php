4f769e25f623b867085f76f2b7ceb87c666c639f
admin <admin@ETUDIANT.local>
2018-11-21 10:44:16 +0100

    Add detail route items

--- config/route.php
+++ config/route.php
@@ -6,4 +6,11 @@ return [
         'path' => '/',
         'controller' => App\Controller\HomeController::class
     ],
+    'article_details' => [
+        'path' => '/article_details/{id}',
+        'controller' => App\Controller\ArticlesDetailsController::class,
+        'params' => [
+            'id' => '\d+'
+        ]
+    ],
 ];
