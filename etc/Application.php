280984f7883473960ce45934761f95fa5562d3e6
admin <admin@ETUDIANT.local>
2018-11-28 16:41:25 +0100

    Add container configuration

--- etc/Application.php
+++ etc/Application.php
@@ -7,5 +7,24 @@ use DI\Container;
 use DI\ContainerBuilder;
 class Application
 {
+    /**
+     * @var Container @container
+     */
+    private $container;
+
+    /**
+     * @throws \Exception
+     */
+    public function init()
+    {
+        $containerBuilder = new ContainerBuilder();
+        $containerBuilder->useAutowiring(true);
+        $containerBuilder->addDefinitions(__DIR__.'/../configs/dic/database.php');
+        $containerBuilder->addDefinitions(__DIR__.'/../configs/dic/repositories.php');
+        $containerBuilder->addDefinitions(__DIR__.'/../configs/dic/render.php');
+        $containerBuilder->addDefinitions(__DIR__. '/../configs/dic/SwiftMailer.php');
+        $this->container = $containerBuilder->build();
+        $this->initRouter();
+    }
 
 }
