064e98d785aed5d82e3106c956d81b000342e4cb
admin <admin@ETUDIANT.local>
2019-01-25 10:04:40 +0100

    Update mail.php file

--- config/mail.php
+++ config/mail.php
@@ -17,7 +17,7 @@ use Framework\MailHelper;
 
 return[
 
-    'sendgrid.api.key' => di_string('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'),
+    'sendgrid.api.key' => di_string('SENDGRID_APY_KEY'),
 
     MailHelper::class => di_object(MailHelper::class)->constructor(
         di_get('sendgrid.api.key')
